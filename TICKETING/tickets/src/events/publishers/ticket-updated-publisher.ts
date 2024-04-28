8a836b14dbb29040845c537874f2f143c3993803
AmeyaBhave28UMBC <AMEYAB1@UMBC.EDU>
2024-04-28 10:52:16 -0400

    Order service created
    Order service created

---
+++ TICKETING/tickets/src/events/publishers/ticket-updated-publisher.ts
@@ -0,0 +1,5 @@
+import { Publisher, Subjects, TicketUpdatedEvent } from '@ab2895tickets/common';
+
+export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+}
